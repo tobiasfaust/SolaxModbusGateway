edca33a81fc49d4dad9faf5c12fb66556e6e2e24
tobiasfaust <tobias.faust@gmx.net>
2023-01-18 10:40:43 +0100

    add direction control pin

--- examples/test_RS485_Solax_ESP32/test_RS485_Solax_ESP32.ino
+++ examples/test_RS485_Solax_ESP32/test_RS485_Solax_ESP32.ino
@@ -1,49 +1,30 @@
 /*
-Anemometer with a RS485 wind sensor
+Script to test a RS485 connection
 
 from an idea of https://arduino.stackexchange.com/questions/62327/cannot-read-modbus-data-repetitively
 https://www.cupidcontrols.com/2015/10/software-serial-modbus-master-over-rs485-transceiver/
 
-_________________________________________________________________
-|                                                               |
-|       author : Philippe de Craene <dcphilippe@yahoo.fr        |
-|       Any feedback is welcome                                 |
-                                                                |
-_________________________________________________________________
-
-Materials :
-• 1* Arduino Uno R3 - tested with IDE version 1.8.7 and 1.8.9
-• 1* wind sensor - RS485 MODBUS protocol of communication
-• 1* MAX485 DIP8
-
-Versions chronology:
-version 1 - 7 sept  2019   - first test 
-
 */
 
-//#include <SoftwareSerial.h>  // https://github.com/PaulStoffregen/SoftwareSerial
+#define RX         4      //Serial Receive pin
+#define TX        15      //Serial Transmit pin
+#define RTS_pin    5      //RS485 Direction control
+#define RS485Transmit    HIGH
+#define RS485Receive     LOW
 
-//#define RX       17      // D1 =5  // 10    //Serial Receive pin
-//#define TX      16      //  D2 = 4  //Serial Transmit pin
-//#define RTS_pin    9    //RS485 Direction control
-//#define RS485Transmit    HIGH
-//#define RS485Receive     LOW
-
-//SoftwareSerial RS485Serial(RX, TX); -> Arduino Mega = Serial2
  
 void setup() {
    
-//  pinMode(RTS_pin, OUTPUT);  
+  pinMode(RTS_pin, OUTPUT);  
   
   // Start the built-in serial port, for Serial Monitor
   Serial.begin(115200);
-  Serial.println("Anemometer"); 
-  Serial.println("Anemometer"); 
-  Serial.println("Anemometer"); 
+  Serial.println("Test RS485 Connection"); 
+  Serial.println("Test RS485 Connection"); 
+  Serial.println("Test RS485 Connection"); 
 
-  // Start the Modbus serial Port, for anemometer
-  Serial2.begin(19200);   
-  //RS485Serial.begin(19200);
+  // Start the Modbus serial Port
+  Serial2.begin(19200, SERIAL_8N1, RX, TX);   
   delay(1000);
 }
 
@@ -75,9 +56,6 @@ String Hex2String(uint8_t num) {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
-  Serial.println("Schreibe Daten ....");
   
   byte request[] = {0x01, 
                                0x03, 
@@ -97,33 +75,24 @@ void loop() {
     Serial.print(Hex2String(request[i]));Serial.print(' ');
   }
   Serial.println("");
-    
-  Serial2.write(request, sizeof(request));
-  Serial2.flush();
-  
+
+      
+  Serial.println("Schreibe Daten ....");
+  digitalWrite(RTS_pin, RS485Transmit);
   delay(100);
 
-/*  if (Serial2.available()) {
-    byte Anemometer_buf[19];
-    Serial2.readBytes(Anemometer_buf, 19);
-  
-    Serial.print("Response : ");
-    for( byte i=0; i<18; i++ ) {
-      Serial.print(Anemometer_buf[i], HEX);
-      Serial.print(" ");
-    }
-    Serial.println(" ");
-  } else {
-    Serial.println("keine Daten da");
-  }
- */
+  Serial2.write(request, sizeof(request));
+  Serial2.flush();
+  delay(100);
 
   Serial.println("Lese Daten ....");
+  digitalWrite(RTS_pin, RS485Receive);
+  
   while(Serial2.available()) 
   {
     Serial.print(Serial2.read(), HEX);
     Serial.print(" ");
   }
  
-  delay(1000);
+  delay(2000);
 }
