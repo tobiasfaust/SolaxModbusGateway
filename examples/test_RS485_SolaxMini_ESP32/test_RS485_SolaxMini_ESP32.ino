1a62685eede0a143b7393ee22d58f88875122603
tobiasfaust <tobias.faust@gmx.net>
2023-01-29 14:16:42 +0100

    test script for Solax Mini

---
+++ examples/test_RS485_SolaxMini_ESP32/test_RS485_SolaxMini_ESP32.ino
@@ -0,0 +1,100 @@
+/*
+Script to test a RS485 connection
+
+from an idea of
+https://arduino.stackexchange.com/questions/62327/cannot-read-modbus-data-repetitively
+https://www.cupidcontrols.com/2015/10/software-serial-modbus-master-over-rs485-transceiver/
+
+*/
+
+#include <Arduino.h>
+
+#define RX         16   // UART2 RX
+#define TX         17   // UART2 TX
+#define RTS_pin    5    // RS485 Direction control
+#define RS485Transmit    HIGH
+#define RS485Receive     LOW
+
+
+
+
+void setup() {
+
+  pinMode(RTS_pin, OUTPUT);
+  digitalWrite(RTS_pin, RS485Receive);
+
+  // Start the built-in serial port, for Serial Monitor
+  Serial.begin(115200);
+  Serial.println("Test RS485 Connection");
+  
+  // Start the Modbus serial Port
+  Serial2.begin(19200, SERIAL_8N1, RX, TX);
+  delay(1000);
+  
+  while(Serial2.available())
+  {
+    Serial.print(Serial2.read(), HEX);
+    Serial.print(" ");
+  }
+  
+  delay(1000);
+}
+
+
+uint16_t Calc_CRC(uint8_t* message, uint8_t len) {
+  // Calc the raw_msg_data_byte CRC code
+  // special CRC computation for Solax Mini
+  
+  uint8_t i;
+  uint16_t checksum = 0;
+  for (i = 0; i <= len; i++) {
+    checksum = checksum + message[i];
+  }
+  return checksum;
+}
+
+String Hex2String(uint8_t num) {
+  char hexCar[2];
+
+  sprintf(hexCar, "0x%02X", num);
+  return hexCar;
+}
+
+void loop() {
+  // request request the device info of Solax Mini
+  byte request[] = {0xAA, 0x55, 
+                    0x01, 0x00,
+                    0x00, 0x0A,
+                    0x11,
+                    0x03,
+                    0x00,
+                    0x00,
+                    0x00
+           }; //
+
+  uint16_t crc = Calc_CRC(request, sizeof(request)-2);
+  request[sizeof(request)-1] = highByte(crc);
+  request[sizeof(request)-2] = lowByte(crc);
+
+  Serial.println("Schreibe Daten ....");
+  for(uint8_t i=0; i<sizeof(request); i++){
+    Serial.print(Hex2String(request[i]));Serial.print(' ');
+  }
+  Serial.println("");
+  
+  digitalWrite(RTS_pin, RS485Transmit);
+  Serial2.write(request, sizeof(request));
+  Serial2.flush();
+  digitalWrite(RTS_pin,RS485Receive);
+  
+  delay(100);
+  Serial.println("Lese Daten ....");
+  
+  while(Serial2.available())
+  {
+    Serial.print(Serial2.read(), HEX);
+    Serial.print(" ");
+  }
+  Serial.println("");
+  delay(2000);
+}
