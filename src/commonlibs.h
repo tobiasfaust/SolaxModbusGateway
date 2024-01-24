60117579466b80f74af7d7a02a087f16718d9e88
tobiasfaust <tobias.faust@gmx.net>
2024-01-24 11:50:55 +0100

    fix LittleFS bug

--- src/commonlibs.h
+++ src/commonlibs.h
@@ -11,23 +11,17 @@
 
 #include <string.h>
 #include <FS.h>
-#include <LITTLEFS.h>
+#include <LittleFS.h>
 
 #ifdef ESP8266
   extern "C" {
       #include "user_interface.h"
   }
   
-  //#include <ESP8266WebServer.h>
-  //#include <ESP8266HTTPUpdateServer.h>
-  //#include <ESP8266mDNS.h>
   #include <ESP8266WiFi.h>
   #include <ESPAsyncTCP.h>
 #elif ESP32
-//  #include "SPIFFS.h"
   #include <WiFi.h> 
-  //#include <ESPmDNS.h>
-  //#include <ESPHTTPUpdateServer.h>
   #include <AsyncTCP.h>
 #endif
 
