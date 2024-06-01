d70c79c7743d47e125c83bea8b6496a2913fd39a
Lazgar <34341913+Lazgar@users.noreply.github.com>
2024-06-01 12:23:47 +0200

    Update modbus.h
    Pin für den Potentialfreien Kontakt

--- src/modbus.h
+++ src/modbus.h
@@ -61,6 +61,8 @@ class modbus {
     uint8_t                 pin_RX;               // Serial Receive pin
     uint8_t                 pin_TX;               // Serial Transmit pin
     uint8_t                 pin_RTS;              // Direction control pin
+    uint8_t                 pin_Relais1;          // Pin zum Abfragen des ersten Potetialfreien Kontakts
+    uint8_t                 pin_Relais2;          // Pin zum Abfragen des ersten Potetialfreien Kontakts
     uint8_t                 default_pin_RX;       // Serial Receive pin
     uint8_t                 default_pin_TX;       // Serial Transmit pin
     uint8_t                 default_pin_RTS;      // Direction control pin
