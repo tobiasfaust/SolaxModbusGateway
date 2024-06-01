aa353c1e1a1074a146e60969ea7a40303af1d15c
Lazgar <34341913+Lazgar@users.noreply.github.com>
2024-06-01 20:18:52 +0200

    Update modbus.cpp

--- src/modbus.cpp
+++ src/modbus.cpp
@@ -29,8 +29,6 @@ modbus::modbus() : Baudrate(19200), LastTxLiveData(0), LastTxIdData(0), LastTxIn
     this->pin_RX = this->default_pin_RX = 16;
     this->pin_TX = this->default_pin_TX = 17;
     this->pin_RTS = this->default_pin_RTS = 5;
-    this->pin_Relais1 = this->default_pin_Relais1 = 18;
-    this->pin_Relais2 = this->default_pin_Relais2 = 19;
   }
 
   this->LoadInvertersFromJson(); //needed for selecting default inverter
@@ -53,8 +51,6 @@ void modbus::init(bool firstrun) {
 
   // Configure Direction Control pin
   pinMode(this->pin_RTS, OUTPUT);
-  pinMode(pin_Relais1, INPUT);
-  pinMode(pin_Relais2, INPUT);
 
   this->LoadInvertersFromJson();
   this->LoadInverterConfigFromJson();
