a879fdf59a15fc1d0c2962010e099febfe0fac4b
Lazgar <34341913+Lazgar@users.noreply.github.com>
2024-06-06 09:12:18 +0200

    Update modbus.cpp

--- src/modbus.cpp
+++ src/modbus.cpp
@@ -1002,7 +1002,7 @@ void modbus::loop() {
     if (this->InverterType.filename.length() > 1) {
       this->QueryLiveData();
       this->state_Relay1 = digitalRead(this->pin_Relay1);
-      if (this->state_Relay1 = 1) {
+      if (this->state_Relay1 == 1) {
       	this->mqtt->Publish_String("relay1",this->state_Relay1, false);
       } else {
         this->mqtt->Publish_String("relay1", this->state_Relay1, false);
