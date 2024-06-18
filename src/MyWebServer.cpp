3a4e4ff68aeca95c1091edeb24b72601a4614f5a
Lazgar <34341913+Lazgar@users.noreply.github.com>
2024-06-18 13:48:49 +0200

    Update MyWebServer.cpp

--- src/MyWebServer.cpp
+++ src/MyWebServer.cpp
@@ -333,7 +333,9 @@ void MyWebServer::GetInitDataStatus(AsyncResponseStream *response) {
   json["data"]["inverter_serial"] = mb->GetInverterSN();
   json["data"]["uptime"] = uptime_formatter::getUptime();
   json["data"]["freeheapmem"] = ESP.getFreeHeap();
-
+  
+  this->mqtt->Publish_String("uptime",uptime_formatter::getUptime(),false);
+  
   json["response"].to<JsonObject>();
   json["response"]["status"] = 1;
   json["response"]["text"] = "successful";
