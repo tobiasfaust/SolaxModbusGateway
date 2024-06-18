9f9d7ab4cc5c8cea46e2608d733b80cc7da84a75
Lazgar <34341913+Lazgar@users.noreply.github.com>
2024-06-18 13:19:21 +0200

    Update mqtt.cpp

--- src/mqtt.cpp
+++ src/mqtt.cpp
@@ -352,6 +352,8 @@ void MQTT::loop() {
 
       snprintf(buffer, sizeof(buffer), "%d", WiFi.RSSI());
       this->Publish_String("rssi", buffer, false);
+
+      this->Publish_String("uptime", uptime_formatter::getUptime(),false);
     }
   }
   
