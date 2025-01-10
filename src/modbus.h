8a0a1f41e09eaac6b4cb578d96995f1a734f37ab
Lazgar <34341913+Lazgar@users.noreply.github.com>
2025-01-10 11:24:35 +0100

    Update modbus.h

--- src/modbus.h
+++ src/modbus.h
@@ -127,6 +127,7 @@ class modbus {
     String                  GetMqttSetTopic(String command);
     void                    ChangeRegItem(std::vector<reg_t>* vector, reg_t item);
     void                    LoadRegItems(std::vector<reg_t>* vector, String type);
+    void                    LoadSetItems(std::vector<setter_t>* vector)
     String                  MapItem(JsonArray map, String value);
     String                  MapBitwise(JsonArray map, String value);
     String                  ConvertIntToBinaryString(int n, int numBits);
