5abd6d5c36d7229d80aa433bd25e5ec3b08a0ab2
Lazgar <34341913+Lazgar@users.noreply.github.com>
2025-01-11 05:49:22 +0100

    Update modbus.cpp

--- src/modbus.cpp
+++ src/modbus.cpp
@@ -1116,7 +1116,7 @@ void modbus::GetSetterAsJson(AsyncWebServerRequest *request) {
             if(*counter > 1) ret += ",";
             ret += "{\"name\": \"" + this->InverterSetData->at(i).Name + "\",";
             ret += "\"realname\": \"" + this->InverterSetData->at(i).RealName + "\",";
-            ret += "\"subscription\": \"" + this->GetMqttSetTopic(this->InverterSetData->at(i).Name, false) + "\"";
+            ret += "\"subscription\": \"" + this->GetMqttSetTopic(this->InverterSetData->at(i).Name) + "\"";
             ret += "\"info\": \"" + this->InverterSetData->at(i).info + "\",";
             
             ret += "}";
