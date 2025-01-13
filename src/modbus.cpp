2a06255d475939618f54d8187433c2a6ea25b7a0
Lazgar <34341913+Lazgar@users.noreply.github.com>
2025-01-13 07:03:43 +0100

    Update modbus.cpp

--- src/modbus.cpp
+++ src/modbus.cpp
@@ -109,7 +109,7 @@ String modbus::GetMqttSetTopic(String command) {
 void modbus::GenerateMqttSubscriptions() {
   for (uint16_t i=0; i < this->InverterSetData->size(); i++) {
     if (this->InverterSetData->at(i).active) {
-      this->mqtt->Subscribe(this->GetMqttSetTopic(this->InverterSetData->at(i).name));
+      this->mqtt->Subscribe(this->GetMqttSetTopic(this->InverterSetData->at(i).Name));
     }
   }
 }
