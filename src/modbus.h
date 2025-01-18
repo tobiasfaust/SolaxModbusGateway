4a4187a97098e900af97d72820868f2b1af314d5
Lazgar <34341913+Lazgar@users.noreply.github.com>
2025-01-18 21:49:43 +0100

    Update modbus.h

--- src/modbus.h
+++ src/modbus.h
@@ -70,7 +70,6 @@ class modbus {
     void                    GetRegisterAsJson(AsyncResponseStream *response);
     void                    SetItemActiveStatus(String item, bool newstate);
     void                    ReceiveMQTT(String topic, int msg);
-    void		    UpdateSubscription();
 
   private:
     uint8_t                 pin_RX;               // Serial Receive pin
