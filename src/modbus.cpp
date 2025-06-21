5de18b22e9aaa549025b3a5d67ce683e6bc95a86
Lazgar <34341913+Lazgar@users.noreply.github.com>
2025-06-22 00:23:37 +0200

    Update modbus.cpp

--- src/modbus.cpp
+++ src/modbus.cpp
@@ -205,11 +205,10 @@ void modbus::ReceiveMQTT(String topic, String msg) {
       //add by Lasgar
       if (!elem["size"].isNull()) {
 	// der letzte Value aus arr auslesen umwandeln und verdoppeln zurück in byte und dem request anhängen (byte number)
-	byte bn = arr[3];
-	int intbn = bn;
-	intbn = intbn * 2;
-	Config->logN(1, "byte number: %s" ,intbn);
-	bn = intbn;
+	byte bn = this->String2Byte(arr[3]);
+	bn = bn * 2;
+	Config->logN(1, "byte number: %s" ,(this->PrintDataFrame(&bn)).c_str());
+	      
 	request.push_back(bn); //keine ahnung ob das so funktioniert :D
 	//size als Array verfügbar machen      
        	JsonArray sizearr = elem["size"].as<JsonArray>();
