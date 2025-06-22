2d7831528b6f542b7c6b61b4cdbc99737de4d7a3
Lazgar <34341913+Lazgar@users.noreply.github.com>
2025-06-22 14:22:11 +0200

    Update modbus.cpp

--- src/modbus.cpp
+++ src/modbus.cpp
@@ -199,7 +199,7 @@ void modbus::ReceiveMQTT(String topic, String msg) {
 	
 	std::vector<String> mparts = splitStringToVector(msg); // Spliten der "msg" in einzelne Strings und in einen Vector laden
 	
-	for (uint8_t z = 0; z < mparts.size(); z++ ) //Schleife zum umwandeln der Strings und anhängen an den request
+	for (uint8_t z = 0; z < mparts.size(); z++ ) {//Schleife zum umwandeln der Strings und anhängen an den request
 
 	  int msgInt = mparts.at(z).toInt(); // atoi(msg.c_str())
 	  byte bytes[4];
@@ -222,6 +222,8 @@ void modbus::ReceiveMQTT(String topic, String msg) {
             request.push_back(bytes[3]);
 	
 	  }
+	
+	}
 	
       } else { // übliche abarbeitung normaler Set Befehle
         // map values if a mapping is specified
